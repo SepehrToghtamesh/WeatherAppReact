1524ca95cee2b247e610180a5661e51c0fd783ac
Mitra Toghtamesh <mitra.toghtamesh.79@gmail.com>
2022-03-16 21:35:43 +0330

    create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+      </div>{" "}
+    </div>
+  );
+}
+
+export default App;
